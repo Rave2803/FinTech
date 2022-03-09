de29d7e98e38b9a2a60ee9d6e6444d604552655a
Smita <vaidyasmita4@gmail.com>
2022-03-09 09:48:10 +0530

    object_reader.py updated by rahul

--- object_reader.py
+++ object_reader.py
@@ -2,6 +2,6 @@ class ObjectReader:
     def __init__(self,filepath) :
             self.filepath="C:\Users\SMITA VAIDYA\PycharmProjects\FinSys\FEATURES\OR.ini"
 
-    def Get_XPATH(logical_name):
+
 
 
